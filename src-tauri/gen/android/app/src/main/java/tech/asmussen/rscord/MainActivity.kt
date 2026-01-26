c313c0b49ab62fc6a0e72dfd15022421be332e5a
jacob <me@jacob-j.com>
2026-01-26 13:51:58 +0100

    feat: initialize android

---
+++ src-tauri/gen/android/app/src/main/java/tech/asmussen/rscord/MainActivity.kt
@@ -0,0 +1,11 @@
+package tech.asmussen.rscord
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
