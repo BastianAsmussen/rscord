86abd2688780de8c8f44452ddf3a1ce0b8ef23a4
Bastian Asmussen <bastian@asmussen.tech>
2026-02-12 13:43:03 +0100

    chore: update member role tables

---
+++ src-backend/migrations/2026-02-12-102732-0000_create_members_roles/down.sql
@@ -0,0 +1,3 @@
+DROP INDEX idx_members_roles_role;
+DROP INDEX idx_members_roles_user;
+DROP TABLE members_roles;
--- src-backend/migrations/2026-02-12-102732-0000_create_users_roles/up.sql
+++
@@ -1,5 +0,0 @@
-CREATE TABLE users_roles(
-    id SERIAL PRIMARY KEY,
-    user_id BIGINT NOT NULL REFERENCES guilds(id),
-    role_id BIGINT NOT NULL REFERENCES users(id)
-);
