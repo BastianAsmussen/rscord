91bc825604db50230fc82ed862ddf904e8272388
Bastian Asmussen <bastian@asmussen.tech>
2026-02-12 13:37:42 +0100

    chore: update user table schema to use user_status enum

--- src-backend/migrations/2026-02-12-081526-0000_create_users/up.sql
+++ src-backend/migrations/2026-02-12-081526-0000_create_users/up.sql
@@ -1,18 +1,19 @@
+CREATE TYPE user_status AS ENUM('online',
+'dnd',
+'idle',
+'offline');
+
 CREATE TABLE users(
-    id SERIAL PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     email VARCHAR(320) NOT NULL UNIQUE,
-    password_digest CHAR(255) NOT NULL,
-    user_handle VARCHAR(32) NOT NULL,
+    password_digest VARCHAR(255) NOT NULL,
+    user_handle VARCHAR(32) NOT NULL UNIQUE,
     display_name VARCHAR(32) NOT NULL,
-    status VARCHAR(255) CHECK(status IN(
-        'online',
-        'dnd',
-        'idle',
-        'offline'
-    )) NOT NULL DEFAULT 'online',
-    icon_url VARCHAR(255) NULL,
-    settings JSONB NOT NULL,
-    email_verified BOOLEAN NOT NULL DEFAULT '0',
+    status user_status NOT NULL DEFAULT 'offline',
+    icon_url VARCHAR(255)NULL,
+    settings JSONB NOT NULL DEFAULT '{}',
+    email_verified BOOLEAN NOT NULL DEFAULT FALSE,
+
     created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
-    updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL
+    updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL DEFAULT NOW()
 );
