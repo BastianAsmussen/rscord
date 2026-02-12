3e57ae0312971b8ae23dda82890d840e0be3e533
Bastian Asmussen <bastian@asmussen.tech>
2026-02-12 13:41:25 +0100

    chore: update guilds table with constaints and indicies

--- src-backend/migrations/2026-02-12-101329-0000_create_guilds/up.sql
+++ src-backend/migrations/2026-02-12-101329-0000_create_guilds/up.sql
@@ -1,7 +1,13 @@
 CREATE TABLE guilds(
-    id SERIAL PRIMARY KEY,
+    id BIGSERIAL PRIMARY KEY,
     owner_id BIGINT NOT NULL REFERENCES users(id),
     name VARCHAR(64) NOT NULL,
-    icon_url VARCHAR(255) NULL
+    icon_url VARCHAR(255) NULL,
+
+    created_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
+    updated_at TIMESTAMP(0) WITHOUT TIME ZONE NOT NULL DEFAULT NOW(),
+
+    CONSTRAINT fk_owner FOREIGN KEY (owner_id) REFERENCES users(id)
 );
 
+CREATE INDEX idx_guilds_owner ON guilds (owner_id);
