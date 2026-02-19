55fc23d46e06d0ab728bb73f1603d4d8402d7539
Bastian Asmussen <bastian@asmussen.tech>
2026-02-19 14:47:22 +0100

    chore: add user model

--- src-backend/src/db/models.rs
+++ src-backend/src/db/models.rs
@@ -1,8 +1,21 @@
+use chrono::NaiveDateTime;
 use diesel::prelude::*;
+use serde::{Deserialize, Serialize};
 
-#[derive(Queryable, Selectable)]
+#[derive(Debug, Serialize, Deserialize, Queryable, Selectable)]
 #[diesel(table_name = crate::db::schema::users)]
 #[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct User {
     pub id: i64,
+
+    email: String,
+    password_digest: String,
+    #[diesel(column_name = user_handle)]
+    handle: String,
+
+    settings: serde_json::Value,
+    email_verified: bool,
+
+    created_at: NaiveDateTime,
+    updated_at: NaiveDateTime,
 }
