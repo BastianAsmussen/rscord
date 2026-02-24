dc87a64e4ed39dcd1380260c9d3fe87bb67e1630
Matkip0 <biegel987@gmail.com>
2026-02-24 09:39:26 +0100

    test: temporary function to get and test push token

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,29 @@
 import { createSignal } from "solid-js";
 import logo from "./assets/logo.svg";
 import { invoke } from "@tauri-apps/api/core";
+import { registerForPushNotifications } from '@choochmeque/tauri-plugin-notifications-api';
 import "./App.css";
 
 function App() {
   const [greetMsg, setGreetMsg] = createSignal("");
   const [name, setName] = createSignal("");
 
+  async function get_push_token(){
+      try {
+          const token = await registerForPushNotifications();
+          console.log('Push token:', token);
+          // Send this token to your server to send push notifications
+          return token
+      } catch (error) {
+          console.error('Failed to register for push notifications:', error);
+          return "epic fail"
+      }
+  }
+
+
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
+      get_push_token()
     setGreetMsg(await invoke("greet", { name: name() }));
   }
 
