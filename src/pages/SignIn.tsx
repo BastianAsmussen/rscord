98826f685d9e685013726691236d09f339f94517
Casper Agerskov Madsen <casperamadsen@gmail.com>
2026-03-02 09:29:19 +0100

    feat: added form and fields for login

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -23,6 +23,48 @@ export default function SignIn() {
                     border border-[#45475a]
                 "
             >
+                <h1 class="text-4xl md:text-5xl font-bold text-center mb-10 text-[#f38ba8]">
+                    Sign In
+                </h1>
+
+                <form onSubmit={login} class="flex flex-col gap-6 md:gap-8">
+                    <input
+                        type="text"
+                        placeholder="Username"
+                        value={username()}
+                        onInput={(e) => setUsername(e.currentTarget.value)}
+                        class="
+                            w-full
+                            px-5 py-3
+                            text-white
+                            placeholder:text-[#a6adc8]
+                            rounded-2xl
+                            bg-[#45475a]
+                            border border-[#585b70]
+                            focus:border-[#f38ba8]
+                            focus:outline-none
+                        "
+                        required
+                    />
+
+                    <input
+                        type="password"
+                        placeholder="Password"
+                        value={password()}
+                        onInput={(e) => setPassword(e.currentTarget.value)}
+                        class="
+                            w-full
+                            px-5 py-3
+                            text-white
+                            placeholder:text-[#a6adc8]
+                            rounded-2xl
+                            bg-[#45475a]
+                            border border-[#585b70]
+                            focus:border-[#f38ba8]
+                            focus:outline-none
+                        "
+                        required
+                    /></form>
             </div>
         </div>
     );
