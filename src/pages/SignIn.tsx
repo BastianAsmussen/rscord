83624a763d038ca3d744723126017f86a10e798d
Casper Agerskov Madsen <casperamadsen@gmail.com>
2026-03-02 09:28:30 +0100

    feat: added main card styling

--- src/pages/SignIn.tsx
+++ src/pages/SignIn.tsx
@@ -9,4 +9,21 @@ export default function SignIn() {
         e.preventDefault();
         console.log("Login:", username(), password());
     };
+
+    return (
+        <div class="min-h-screen flex items-center justify-center bg-[#0f1220] px-4">
+            <div
+                class="
+                    w-full
+                    max-w-2xl
+                    bg-[#313244]
+                    p-8 md:p-12
+                    rounded-3xl
+                    shadow-2xl
+                    border border-[#45475a]
+                "
+            >
+            </div>
+        </div>
+    );
 }
