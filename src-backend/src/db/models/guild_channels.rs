1e366ec0db23af77b5892625162faf69b77228c0
jacob <me@jacob-j.com>
2026-03-08 17:26:20 +0100

    feat: add channel models for guild and private

---
+++ src-backend/src/db/models/guild_channels.rs
@@ -0,0 +1,32 @@
+﻿use crate::db::schema::guild_channels;
+use crate::db::models::channels::ChannelType;
+use diesel::prelude::*;
+use serde::{Deserialize, Serialize};
+use utoipa::ToSchema;
+
+#[derive(Debug, Serialize, Queryable, Selectable, ToSchema)]
+#[diesel(table_name = crate::db::schema::guild_channels)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
+pub struct GuildChannel {
+    pub id: i64,
+    pub guild_id: i64,
+    pub name: String,
+    #[serde(rename = "type")]
+    pub type_: ChannelType,
+    pub topic: Option<String>,
+    pub position: i32,
+    pub created_at: chrono::NaiveDateTime,
+    pub updated_at: chrono::NaiveDateTime,
+}
+
+#[derive(Debug, Deserialize, Insertable, ToSchema)]
+#[diesel(table_name = guild_channels)]
+pub struct NewGuildChannel {
+    #[serde(skip_deserializing)]
+    pub guild_id: i64,
+    pub name: String,
+    #[serde(rename = "type")]
+    pub type_: ChannelType,
+    pub topic: Option<String>,
+    pub position: Option<i32>,
+}
