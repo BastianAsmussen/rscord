909d25e753d78b2cb80ae62a0375b5dce9038a00
Bastian Asmussen <bastian@asmussen.tech>
2026-03-13 11:12:45 +0100

    chore: remove unused function

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -4,12 +4,6 @@ use crate::api::{
     push_token::add_push_token,
 };
 
-// Learn more about Tauri commands at https://tauri.app/develop/calling-rust/
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
-}
-
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
@@ -17,7 +11,6 @@ pub fn run() {
         .plugin(tauri_plugin_opener::init())
         .plugin(tauri_plugin_notifications::init())
         .invoke_handler(tauri::generate_handler![
-            greet,
             add_push_token,
             sign_up,
             log_in
