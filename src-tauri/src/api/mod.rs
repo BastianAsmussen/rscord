a872be11c056b11515412e1ca15a3ad749cd8f33
Bastian Asmussen <bastian@asmussen.tech>
2026-03-17 09:39:29 +0100

    chore: add real domain of vps server

--- src-tauri/src/api/mod.rs
+++ src-tauri/src/api/mod.rs
@@ -5,6 +5,5 @@ pub mod push_token;
 pub mod token;
 pub mod ws_client;
 
-// TODO: needs to be updated when we get server
-pub const BASE_URL: &str = "http://127.0.0.1:8080";
-pub const WS_URL: &str = "ws://127.0.0.1:8080";
+pub const BASE_URL: &str = "http://rscord.asmussen.tech";
+pub const WS_URL: &str = "ws://rscord.asmussen.tech";
